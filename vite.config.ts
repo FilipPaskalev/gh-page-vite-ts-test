b8bad7b0fb1a4e69c44764ac54274510f16a6a5c
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-22 19:48:08 +0100

    Update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 export default defineConfig({
   base: '/gh-page-vite-ts-test/',
   plugins: [react()],
+  define: {
+    'process.env': process.env,
+  },
 })
